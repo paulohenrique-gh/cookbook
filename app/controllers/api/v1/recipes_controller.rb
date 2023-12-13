11d2dd7685deffb4ca471aeadce9bfdeba94a0c6
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2023-12-13 16:09:19 -0300

    Adicionado controller para busca de receitas na API

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::RecipesController < Api::V1::RecipesController
   def search
-
+    @recipe = Recipe.where('title LIKE ?', params[:query])
   end
 end
--- spec/requests/recipe_search_api_spec.rb
+++ spec/requests/recipe_search_api_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 describe 'Busca de receitas API' do
+  context 'GET /api/v1/search' do
+    recipe_type = RecipeType.create!(name: 'Sobremesa')
 
+    get '/api/v1/search/'
+  end
 end
